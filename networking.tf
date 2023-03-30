5fdcaeed5d409d268d9e513a4a315a3352db27ce
repothatapp <129278824+repothatapp@users.noreply.github.com>
2023-03-30 23:11:50 +0530

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "appnetwork" {
   name                = local.virtual_network.name
   location            = local.location  
   resource_group_name = local.resource_group_name
-  address_space       = [local.virtual_network.address_space]
+  address_space       = local.virtual_network.address_space
   depends_on = [
     azurerm_resource_group.appgrp
   ]  
